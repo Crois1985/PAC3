faad02654ed1b96124fa53c5ab25a345e168ba6f
Crois1985 <crois85@gmail.com>
2023-05-13 15:33:53 +0200

    Motor principal

--- codi.js
+++ codi.js
@@ -19,35 +19,74 @@ function get_rnd(n,max)
 
 
 
-async function crear_llista(pkm_ids)
+async function start()
 {
     //Creem els divs necessaris a partir de la llista pkm_ids
-    //pkm_ids= objecte array amb els ids dels pokemos
+    
+  
+    if (window.location.href.includes('?pokeID')){
+        let params = new URLSearchParams(document.location.search);
+        let pk_id = params.get('pokeID')
+        console.log()
 
-    for (const pk_id of pkm_ids) //Per a cada pokemon li creem una previsualització
-    {
-        var pkm=document.createElement('div');
-        var pkm_img=document.createElement('img');
-        var pkm_name=document.createElement('h3');
-        pkm.setAttribute("id", "pkm_" + pk_id);
-        pkm.setAttribute("class","card");
-        document.getElementById("llistat").appendChild(pkm);
-        document.getElementById("pkm_" + pk_id).appendChild(pkm_img);
-        document.getElementById("pkm_" + pk_id).appendChild(pkm_name);
-
-
-
-       //Obtenim la info dels pokemons
-
-        var info={}; //Diccionari amb la info del pokemon
         var pkm_id="https://pokeapi.co/api/v2/pokemon/"+pk_id;
         var pkm_info=await fetch(pkm_id);
     
         const data= await pkm_info.json();
         
-        
-        pkm_name.innerText=data.name;
-        pkm_img.setAttribute("src",data.sprites.front_default);
+        console.log(data.name)
+      //  pkm_name.innerText=data.name;
+      //  pkm_img.setAttribute("src",data.sprites.front_default);
 
     }
+
+        //pkm_ids= objecte array amb els ids dels pokemos
+        //Comprovem que no estiguin salvats a la sessio...si ho estan...els recuperem
+        if (sessionStorage.hasOwnProperty("ids")==false)
+        {
+            console.log("no pokemons");
+            pkm_ids=get_rnd(10,1000)
+            sessionStorage.setItem("ids", pkm_ids);
+        }
+         else {
+            console.log("Si pokemons");
+            pkm_ids_txt=sessionStorage.getItem("ids");
+            pkm_ids=pkm_ids_txt.split(',');
+            console.log(pkm_ids)
+         }   
+
+
+//Per a cada pokemon li creem una previsualització
+        for (const pk_id of pkm_ids) 
+        {
+            var pkm=document.createElement('div');
+            var pkm_img=document.createElement('img');
+            var pkm_name=document.createElement('h3');
+            var lnk=document.createElement('a');
+            lnk.setAttribute("id", "a_" + pk_id);
+            pkm.setAttribute("id", "pkm_" + pk_id);
+            pkm.setAttribute("class","card");
+
+            document.getElementById("llistat").appendChild(lnk);
+            document.getElementById("a_" + pk_id).appendChild(pkm);
+            
+            document.getElementById("pkm_" + pk_id).appendChild(pkm_img);
+            document.getElementById("pkm_" + pk_id).appendChild(pkm_name);
+            
+            
+            var pkm_a="index.html?pokeID="+pk_id;
+            lnk.setAttribute("href",pkm_a);  //Afegim la url al link
+
+        //Obtenim la info dels pokemons per la llsita
+            
+           var pkm_id="https://pokeapi.co/api/v2/pokemon/"+pk_id;
+           // console.log(pkm_id);
+            var pkm_info=await fetch(pkm_id);
+            const data= await pkm_info.json();
+
+            pkm_name.innerText=data.name;
+            pkm_img.setAttribute("src",data.sprites.front_default);
+
+        }
+    
 }
